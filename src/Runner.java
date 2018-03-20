966260f4a28600a083b0dc8a73c672ee961e3dc7
pc <pc@DESKTOP-7TQM1P4>
2018-03-20 14:51:38 +0200

    First commit - starting project

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,7 @@
 public class Runner {
         public static void main(String[] args) {
             System.out.println("How atre you doing");
+            System.out.println("How atre you doing");
+
     }
 }
