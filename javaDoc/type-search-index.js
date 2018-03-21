dc998d6f77e4b12423a32f4c3a336aba1b431e67
pc <pc@DESKTOP-7TQM1P4>
2018-03-21 21:03:48 +0200

    Added javaDocs

---
+++ javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"ImgTag"},{"p":"<Unnamed>","l":"LinesInFile"},{"p":"<Unnamed>","l":"LinkTag"},{"p":"<Unnamed>","l":"ManageStatistics"},{"p":"<Unnamed>","l":"Runner"},{"p":"<Unnamed>","l":"Statistable"},{"p":"<Unnamed>","l":"Statistics"}]
